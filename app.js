2f6c99d165859499b492a1a9a07ad9e165c2201e
Thurbers <rexbrg@gmail.com>
2021-03-28 21:11:09 -0600

    Sun night push

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ let clicks = 0
 
 
 
-let clickUpgrades = {
-    pickaxes: {
-      price: 100,
-      quantity: 0,
-      multiplier: 1
-    }
-  }
+// let clickUpgrades = {
+//     pickaxes: {
+//       price: 100,
+//       quantity: 0,
+//       multiplier: 1
+//     }
+//   }
 
 
 
@@ -74,61 +74,66 @@ let levelTools = [
 
 
   function superMixer(id){
-      if (clickCoin >= levelTools[0].price) {
-          clickCoin = clickCoin - levelTools[0].price;
-          levelTools[0].quantity = levelTools[0].quantity +1;
-          levelTools[0].modifier = levelTools[0].modifier +1;
-          levelTools[0].price = levelTools[0].price * levelTools[0].modifier;
-          levelTools[0].quantity = levelTools[0].quantity + levelTools[0].modifier;
-        //   clickCoin = clickCoin * levelTools[0].modifier;
-          moreCoin = levelTools[0].modifier;
+      if (clickCoin >= levelTools[id].price) {
+
+          clickCoin = clickCoin - levelTools[id].price;
+          levelTools[id].modifier = levelTools[id].modifier +1;
+          levelTools[id].price = levelTools[id].price * levelTools[id].modifier;
+          levelTools[id].quantity = levelTools[id].quantity +1;
+
+          upClick = clickCoin
+          moreCoin = levelTools[id].modifier;
+
+
           document.getElementById("ClickerBuy").innerHTML  
-          = levelTools[0].modifier +" "+ `ClickCoins Per click!`;
+          = levelTools[id].modifier +" "+ `ClickCoins Per click!`;
         console.log(clickCoin, "CC beore mod")
         // clickCoin = clickCoin * levelTools[0].modifier
         } else{
             alert("Not Enough Click Coins!")
+        } if(levelTools[id] === levelTools[1]){
+            startInterval()
+
         }
 
         // clickCoin = clickCoin * levelTools[0].modifier
 
-        // console.log(levelTools[0].price, '****** PRICE')
-        // console.log(levelTools[0].modifier, "Modifier")
-        // console.log(levelTools[0].quantity, "Q")
-        // console.log(levelTools[0].price, "NEW PRICE")
-        console.log(clickCoin, "New CLICKER VAL ****************")
+        console.log(id)
+        // clickCookie()
         update()
+  }
+
+
+  function startInterval() {
+    collectionInterval = setInterval(collectAutoUpgrades, 3000);
   }
 
 
   // TODO buySuperMixer
   // check if they have enough resource (clickCoin)+
   //      reduce resoure by item price+
-  //      increase item price
-  //      increase item quantity
+  //      increase item price+
+  //      increase item quantity+
   //      draw item updates
   //       call update(   `)
 
 
 
-function upClick(){
-
-}
 
 
 
-function clickCookie(){
-    clicks = clickCoin + moreCoin;
+
+function clickCookie(id){
     clickCoin++;
+    // upClick = clickCoin * levelTools[id].quantity;
     moreCoin++;
     newClickCoin =0;
 
 
-    // if(levelTools[0].modifier>=1){
-    //     newClickCoin = levelTools[0].modifier * levelTools[0].quantity;
-    //     clickCoin = newClickCoin
+    // if(levelTools[id].modifier>=1){
+    //     clickCoin = clickCoin * levelTools[id].modifier ;
     // }
-    // newClickCoin = newClickCoin ++;
+    // newClickCoin = clickCoin;
     // TODO add modifiers of clicks
     //      filter tools to only !auto tools
     // itterate over that list to clickCoin the quantity * modifier
@@ -141,7 +146,20 @@ function clickCookie(){
 }
 
 
+function atuoclick(){
+    // TODO autoClick (see clickCookie but change from !auto to auto)
+// set up interval to call 'autoClick'
 
+    setInterval(function(){ alert("Hello"); }, 3000);
+  
+}
+
+
+
+
+ function mixer(){
+     clickCoin = clickCoin *2;
+ }
 
 
 function grandma(){
@@ -173,8 +191,6 @@ function grandma(){
 
 
 
-
-
 function update(){
     document.getElementById('cCount').innerText = clickCoin;
     // document.getElementById('modifier1').innerText = levelTools[0].modifier;
@@ -192,11 +208,11 @@ function drawTools(){
     let toolTemplate='';
         levelTools.forEach(levelUp =>{
             toolTemplate +=`<div class="col sidenav">
-            <img src="${levelUp.stopImg}" class="level_up" onclick="superMixer('${levelUp.id}')" style="width:90px;height:90px;">
+            <img src="${levelUp.imgUrl}" class="level_up" onclick="superMixer('${levelUp.id}')" style="width:90px;height:90px;">
             <ul>
                 <li><b>Name: ${levelUp.name}</b></li>
-                <li>Price:${levelUp.price}</li>
-                <li> ${levelUp.modifier}  :Number of ${levelUp.name}</li>
+                <li><h3>Price:${levelUp.price}</h3></li>
+                <li> ${levelUp.quantity}  :Number of ${levelUp.name}</li>
             </ul>
             <hr><BR>
             </div>`
@@ -209,13 +225,7 @@ function drawTools(){
 
     
     
-    function atuoclick(){
-        // TODO autoClick (see clickCookie but change from !auto to auto)
-    // set up interval to call 'autoClick'
-
-}
 
 
 update()
 drawTools()
-
